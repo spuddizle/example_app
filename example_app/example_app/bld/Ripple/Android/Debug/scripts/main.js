b0123f7da1f22b8bace65d184616829ca7a0450c
spuddizle <spuddizle@hotmail.com>
2014-11-20 11:06:17 +1300

    phonegap injection
    adding phonegap.js and alerts

--- example_app/example_app/bld/Ripple/Android/Debug/scripts/main.js
+++ example_app/example_app/bld/Ripple/Android/Debug/scripts/main.js
@@ -1,6 +1,7 @@
 var app = {
 
-    findByName: function() {
+    findByName: function ()
+    {
         console.log('findByName');
         this.store.findByName($('.search-key').val(), function(employees) {
             var l = employees.length;
@@ -13,8 +14,25 @@ var app = {
         });
     },
 
-    initialize: function() {
-        this.store = new MemoryStore();
+    showAlert: function (message, title)
+    {
+        if (navigator.notification)
+        {
+            navigator.notification.alert(message, null, title, 'OK');
+        }
+        else
+        {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    initialize: function ()
+    {
+        var self = this;
+        this.store = new MemoryStore(function ()
+        {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
